4e50777ecb1cdb3b46e8d05fa461484c4736e785
Khang <khangluong47@gmail.com>
2022-06-30 12:00:11 -0700

    added a second line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
     public static void main(String args[]){
         System.out.println("Hello World My name is khang. adding more. i like apple");
+        System.out.println("Again hello!");
     }
 }
