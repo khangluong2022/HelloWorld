f781fc2739027d564ab678ceeb22b91344b3e51e
Khang Luong <khangluong47@gmail.com>
2022-07-15 10:40:31 -0700

    testing

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
     public static void main(String args[]){
         System.out.println("Hello World My name is khang. adding more. i like apple");
         System.out.println("Again hello!. Attempting local");
+        System.out.println("My name it github tester");
     }
 }
